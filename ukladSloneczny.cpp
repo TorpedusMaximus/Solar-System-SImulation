93addc419a32bfab2b0cf54330defe5269e326f8
malko <msa>
2022-01-25 15:38:56 +0100

    comments

--- ukladSloneczny.cpp
+++ ukladSloneczny.cpp
@@ -19,54 +19,54 @@ using namespace std;
 typedef float point3[3];
 typedef float point9[9];
 
-static GLfloat viewer[] = { 0.0, 500.0, 0.0 , 0.0, 0.0 , 0.0 };
+static GLfloat viewer[] = { 0.0, 500.0, 0.0 , 0.0, 0.0 , 0.0 };//sterowanie kamera
 static GLfloat azymuth = 0;
 static GLfloat elevation = 0;
-static GLfloat azymuth2 = 0;
-static GLfloat elevation2 = 0;
 
-static int x_pos_old = 0;
+static int x_pos_old = 0;//szczegoly sterowania kamera
 static int delta_x = 0;
 static int y_pos_old = 0;
 static int delta_y = 0;
+int r = 10;
 static GLfloat pix2angle;
 
-static GLint statusMiddle = 0;
+static GLint statusMiddle = 0;//statusy 
 static GLint statusLeft = 0;
 static GLint statusRight = 0;
+bool statusOrbits = 0;
+bool show = true;
+bool click = false;
 
-int r = 10;
-int days[] = { 88,224,365,686,4333,10756,30707, 60223 };
-int speed = 1;
+int speed = 1;//sterowanie symulacja
 int day = 0;
+int hour = 0;
 time_t start;
+
+float ring[3600];//tablica do rysowania pierscieni saturna
+int segments = 40;//dokladnosc obiektow
+
+int days[] = { 88,224,365,686,4333,10756,30707, 60223 };//dane dotyczadce rysowanych obiektow
 int radius[] = { 57,108,149,227,778,1433,2872,4498 };
 float planetSize[] = { 4.87, 12.1, 12.76, 6.79, 71, 60, 25, 24, 30 };
-bool simulation = false;
-float ring[3600];
-int segments = 40;
 double rings[] = { 102,102.3,102.6,103,103.8,104,110,115,119,125,132,136,141,146,149,155,156,158,160,165 };
 GLfloat planetTilt[] = { 0, 177.3, 23.4, 25.2, 3.1, 26.7, 97.8, 28.3, 0 };
 GLfloat rotation[] = { 1407, 5832, 24, 24, 10, 11, 17, 16, 625 };
 GLfloat orbitsEccentrity[] = { 0.206, 0.007, 0.017, 0.093, 0.048, 0.056, 0.046, 0.010 };
-GLbyte* textures[10];
+
+GLbyte* textures[10];//dane tekstur
 GLint ImWidth[10], ImHeight[10], ImComponents[10];
 GLenum ImFormat[10];
 GLUquadric* sphere;
-int hour = 0;
-
-bool show = true;
-bool click = false;
 
 
 void texture(int textureID);
 
 void planetRotation(int planetID) {
-	glRotated(-90.0, 1.0, 1.0, 0.0);
+	glRotated(-90.0, 1.0, 1.0, 0.0);//ustawienie odpowiedniego kantu wzgledewm pionu
 
-	glRotated(-planetTilt[planetID], 1.0, 0.0, 0.0);
+	glRotated(-planetTilt[planetID], 1.0, 0.0, 0.0);//kat nachylenia planety
 
-	glRotated(360 * ((((double)day * 24) + hour) / rotation[planetID]), 0.0, 0.0, 1.0);
+	glRotated(360 * ((((double)day * 24) + hour) / rotation[planetID]), 0.0, 0.0, 1.0);//obrot wokol osi
 
 	gluSphere(sphere, planetSize[planetID], segments, segments);
 
@@ -86,9 +86,9 @@ void radiusAngle(int planetID, GLdouble& r, GLdouble& angle) {
 	GLdouble time = (double)(day + ((1.0 * hour) / 24));
 	GLdouble timeMax = (days[planetID]);
 	GLdouble ratio = time / timeMax;
-	angle = 2 * M_PI * ratio;
+	angle = 2 * M_PI * ratio;//okreslenia kanta obrotu wokol slonca
 
-	double e = orbitsEccentrity[planetID];
+	double e = orbitsEccentrity[planetID];//obliczenie parametrow elipsy
 	double baRatio = sqrt(1 - pow(e, 2));
 	double a = radius[planetID];
 	double b = a * baRatio;
@@ -113,10 +113,10 @@ void radiusAngle(int planetID, GLdouble& r, GLdouble& angle) {
 	double top = a * (1 - pow(e, 2));
 	double bottom = 1 + e * cos(angle);
 
-	r = top / bottom;
+	r = top / bottom;//policzenie odleglosci plenty od slonca
 }
 
-GLdouble orbitPoint(int planetID, GLdouble& r, GLdouble ratio) {
+GLdouble orbitPoint(int planetID, GLdouble& r, GLdouble ratio) {//obliczenie kolejnego punktu na elipsie
 	GLdouble angle = 2 * ratio * M_PI;
 
 	double e = orbitsEccentrity[planetID];
@@ -152,9 +152,9 @@ void orbits() {
 }
 
 void sun() {
-	texture(8);
-	glRotated(90, 1.0, 0.0, 0.0);
-	glRotated(-360 * ((((double)day * 24) + hour) / rotation[8]), 0.0, 0.0, 1.0);
+	texture(8);//wybranie tektusy obiektu
+	glRotated(90, 1.0, 0.0, 0.0);//ustawienie odpowiedniego kantu wzgledewm pionu
+	glRotated(-360 * ((((double)day * 24) + hour) / rotation[8]), 0.0, 0.0, 1.0);//obrot wokol osi
 	gluSphere(sphere, planetSize[8], segments, segments);
 	glRotated(360 * (((double)day * 24 + hour) / rotation[8]), 0.0, 0.0, 1.0);
 	glRotated(-90, 1.0, 0.0, 0.0);
@@ -162,39 +162,40 @@ void sun() {
 
 void planets() {
 	for (int i = 0; i < 8; i++) {
-		if (i == 5) {
+		if (i == 5) {//saturn pominiety ze wzgledu na pierscienie
 			continue;
 		}
 
 		GLdouble r, elipseAngle;
-		radiusAngle(i, r, elipseAngle);
+		radiusAngle(i, r, elipseAngle);//obliczenie odleglosci od slonca i aktualnego katu wzgledem punktu starowego orbity
 
 		GLdouble x, y;
-		translateAngle(r, elipseAngle, x, y);
+		translateAngle(r, elipseAngle, x, y);//obliczenie pozycji x i y wzgledem odleglosci i katu
 
 		glTranslated(x, 0, y);
-		texture(i);
-		planetRotation(i);
+		texture(i);//wgranie tekstury
+		planetRotation(i);//obrot
 		glTranslated(-x, 0, -y);
 	}
 }
 
 void saturn() {
 	GLdouble r, elipseAngle;
-	radiusAngle(5, r, elipseAngle);
+	radiusAngle(5, r, elipseAngle);//obliczenie odleglosci od slonca i aktualnego katu wzgledem punktu starowego orbity
 
 	GLdouble x, y;
-	translateAngle(r, elipseAngle, x, y);
+	translateAngle(r, elipseAngle, x, y);//obliczenie pozycji x i y wzgledem odleglosci i katu
 
 	glTranslated(x, 0, y);
-	texture(5);
-	planetRotation(5);
-	glRotated(1.2 * planetTilt[5], -1.0, 0.0, 1.0);
+	texture(5);//wgranie tekstury
+	planetRotation(5);//obrot
+
+	glRotated(1.2 * planetTilt[5], -1.0, 0.0, 1.0);//nachylenie pierscieni
 	for (double i : rings) {
 		glColor3f(1, 1, 1);
 
 		glBegin(GL_LINE_LOOP);
-		for (int ii = 0; ii < 3600; ii++) {
+		for (int ii = 0; ii < 3600; ii++) {//rysowanie pierscienia
 			float angle = 1.0 * float(ii) / float(3600);
 			float xr = i * cos(2 * M_PI * angle);
 			float yr = i * sin(2 * M_PI * angle);
@@ -209,15 +210,15 @@ void saturn() {
 }
 
 void zoom(bool zoom) {
-	float x = (viewer[3] - viewer[0]);
+	float x = (viewer[3] - viewer[0]);//obliczenie wartosci wektorow skladowych
 	float y = (viewer[4] - viewer[1]);
 	float z = (viewer[5] - viewer[2]);
-	if (zoom) {
+	if (zoom) {//sprawdzenie kierunku
 		x *= -1;
 		y *= -1;
 		z *= -1;
 	}
-	viewer[0] += x;
+	viewer[0] += x;//przesuniecie kamery
 	viewer[1] += y;
 	viewer[2] += z;
 	viewer[3] += x;
@@ -228,10 +229,10 @@ void zoom(bool zoom) {
 
 void solarSystem() {
 	if (statusRight == 1) {
-		zoom(delta_y > 0);
+		zoom(delta_y > 0);//wykonanie przyblizenia
 	}
 
-	if (statusLeft == 1) {
+	if (statusLeft == 1) {//obrot kamera "glowa" obserwatora
 		azymuth += ((float)delta_x * pix2angle) * 0.01;
 		elevation -= ((float)delta_y * pix2angle) * 0.01;
 		if (sin(elevation) >= 0.99) {
@@ -241,17 +242,21 @@ void solarSystem() {
 			elevation = -1.44;
 		}
 	}
-	viewer[3] = 10 * cos(azymuth) * cos(elevation) + viewer[0];
+	viewer[3] = 10 * cos(azymuth) * cos(elevation) + viewer[0];//ustalenie pozycji puktu na ktory patrzymy
 	viewer[4] = 10 * sin(elevation) + viewer[1];
 	viewer[5] = 10 * sin(azymuth) * cos(elevation) + viewer[2];
 
-	orbits();
-	sun();
+
+	if (statusOrbits) {//wlaczenie rysowania orbit
+		orbits();
+	}
+
+	sun();//rysowanie obiektow
 	planets();
-	saturn();
+	saturn();//saturn rysowany oddzielnie ze wzgledu na rysowanie pierscieni
 }
 
-void printDay() {
+void printData() {//wypisanie parametrow symulacji na ekranie czas, predkosc, wspolrzedne kamery
 	const int characters = 80;
 	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
@@ -378,20 +383,36 @@ void printDay() {
 	glMatrixMode(GL_MODELVIEW);
 }
 
+void light() {
+	int lightRadius = 50;//ustawienie zrodel swiatla
+	int lightAngle = 60;
+	for (int i = 0; i <= 5; i++) {
+
+		GLfloat x = cos(i * lightAngle) * lightRadius;//obliczenie pozycji zrodel swiatla
+		GLfloat y = -1 * sin(i * lightAngle) * lightRadius;
+		GLfloat LightPosition[] = { x, 0.0f, y, 1.0f };
+		glLightfv(GL_LIGHT0 + i, GL_POSITION, LightPosition); //swiatla wokol 
+	}
+
+	GLfloat topLightPosition[] = { 0, lightRadius, 0, 1.0f };//gorne swiatlo
+	glLightfv(GL_LIGHT6, GL_POSITION, topLightPosition);
+	GLfloat bottomLightPosition[] = { 0, -lightRadius, 0, 1.0f };//dolne swiatlo
+	glLightfv(GL_LIGHT7, GL_POSITION, bottomLightPosition);
+}
+
 void RenderScene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	gluLookAt(viewer[0], viewer[1], viewer[2], viewer[3], viewer[4], viewer[5], 0.0, 1.0, 0.0);
 
-	if (click && show) {
+	if (click && show) {//wylaczenie pomocy 
 		time_t now = clock();
 		if ((now - start) / CLOCKS_PER_SEC >= 3) {
 			show = false;
 		}
 	}
 
-	while (hour >= 24) {
+	while (hour >= 24) {//liczenie dni
 		hour -= 24;
 		day++;
 	}
@@ -400,26 +421,12 @@ void RenderScene(void)
 		day--;
 	}
 
-	solarSystem();
-	printDay();
+	solarSystem();//rysowanie sceny
+	gluLookAt(viewer[0], viewer[1], viewer[2], viewer[3], viewer[4], viewer[5], 0.0, 1.0, 0.0);//ustawienie kamery
+	printData();//wypisanie informacji
+	light();//rysowanie oswietlenia
 
-	int lightRadius = 50;
-	int lightAngle = 60;
-	for (int i = 0; i <= 5; i++) {
-
-		GLfloat x = cos(i * lightAngle) * lightRadius;
-		GLfloat y = -1 * sin(i * lightAngle) * lightRadius;
-		GLfloat LightPosition[] = { x, 0.0f, y, 1.0f };
-		glLightfv(GL_LIGHT0 + i, GL_POSITION, LightPosition);
-	}
-
-	GLfloat topLightPosition[] = { 0, lightRadius, 0, 1.0f };
-	glLightfv(GL_LIGHT6, GL_POSITION, topLightPosition);
-	GLfloat bottomLightPosition[]= { 0, -lightRadius, 0, 1.0f };
-	glLightfv(GL_LIGHT7, GL_POSITION, bottomLightPosition);
-
-
-	glutSwapBuffers();
+	glutSwapBuffers();//rysoawnie wygenerowanej sceny na ekranie
 }
 
 
@@ -461,8 +468,8 @@ void Mouse(int btn, int state, int x, int y)
 
 void keys(unsigned char key, int x, int y)
 {
-	int vectorMultiplier = 7;
-	if (key == 'r') {
+	int vectorMultiplier = 7;//zmienna sluzaca do poruszania sie z uzyciem klawiatury
+	if (key == 'r') {//resetowanie ustawienai kamery do ustawienia poczatkowego
 		viewer[0] = 0;
 		viewer[1] = 500;
 		viewer[2] = 0;
@@ -472,14 +479,23 @@ void keys(unsigned char key, int x, int y)
 
 	}
 
-	if (key == 'u') {
+	if (key == 'o') {//wlaczanie/wylaczanie orbit
+		if (statusOrbits) {
+			statusOrbits = false;
+		}
+		else {
+			statusOrbits = true;
+		}
+	}
+
+	if (key == 'u') {//sterowanie szybkoscia symulacji
 		speed++;
 	}
 	if (key == 'j') {
 		speed--;
 	}
 
-	if (key == 'w') {
+	if (key == 'w') {//poruszanie sie kamera przy uzyciu  WASD
 		double x = viewer[3] - viewer[0];
 		double y = viewer[5] - viewer[2];
 		double vector = sqrt(pow(x, 2) + pow(y, 2));
@@ -521,15 +537,15 @@ void keys(unsigned char key, int x, int y)
 		viewer[2] += vectorMultiplier * x / vector;
 	}
 
-	if (!click) {
+	if (!click) {//wylaczenie samouczka
 		click = true;
 		start = clock();
 	}
-	if (key == 't') {
+
+	if (key == 't') {//resetowanie symualcji
 		day = 0;
+		hour = 0;
 	}
-
-
 
 	RenderScene();
 }
@@ -679,13 +695,11 @@ GLbyte* LoadTGAImage(const char* FileName, GLint* ImWidth, GLint* ImHeight, GLin
 
 }
 
-void texture(int textureID) {//plik to nazwa wczytywanego pliku
-
+void texture(int textureID) {//ustawienie potzrebnej tesktury
 	glTexImage2D(GL_TEXTURE_2D, 0, ImComponents[textureID], ImWidth[textureID], ImHeight[textureID], 0, ImFormat[textureID], GL_UNSIGNED_BYTE, textures[textureID]);//zdefiniowanie tekstury
-
 }
 
-void loadTextures() {
+void loadTextures() {//wczytanie wszytskich tekstur do pamieci
 	const char* plik[] = {
 		"mercury.tga",
 		"venus.tga",
@@ -704,14 +718,14 @@ void loadTextures() {
 	for (auto sciezka : plik) {
 		pBytes = new GLbyte;
 		pBytes = LoadTGAImage(sciezka, &ImWidth[i], &ImHeight[i], &ImComponents[i], &ImFormat[i]);//wczytanie tekstury
-		textures[i] = pBytes;
+		textures[i] = pBytes;//zapis do pamieci
 		i++;
 	}
 
 }
 
 void material() {
-	float MatAmbient[] = { 0.1f, 0.1f, 0.1f, 1.0f };
+	float MatAmbient[] = { 0.1f, 0.1f, 0.1f, 1.0f };//parametry powierzchni
 	float MatDiffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 	float MatSpecular[] = { 0.1f, 0.1f, 0.0f, 0.1f };
 	float MatShininess = 20;
@@ -730,8 +744,8 @@ void material() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);//sposob nakladania tekstur
 }
 
-void light() {
-	GLfloat att_constant = { 0.7 };
+void lightInit() {
+	GLfloat att_constant = { 0.7 };//parametry swiatla
 	GLfloat att_linear = { 0.0000005 };
 	GLfloat att_quadratic = { 0.00000000001 };
 	float LightAmbient[] = { 0.1f, 0.1f, 0.05f, 1.0f };
@@ -739,7 +753,7 @@ void light() {
 	float LightDiffuse[] = { 1.0f, 1.0f, 0.8f, 1.0f };
 	float LightSpecular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 
-	for (int i = 0; i <= 7; i++) {
+	for (int i = 0; i <= 7; i++) {//ustawienie zrodel
 		glLightfv(GL_LIGHT0 + i, GL_AMBIENT, LightAmbient);
 		glLightfv(GL_LIGHT0 + i, GL_DIFFUSE, LightDiffuse);
 		glLightfv(GL_LIGHT0 + i, GL_SPECULAR, LightSpecular);
@@ -755,11 +769,11 @@ void light() {
 
 void MyInit()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);//tlo okna
 	material();
-	light();
+	lightInit();
 	loadTextures();
-	sphere = gluNewQuadric();
+	sphere = gluNewQuadric();//ustawienie bazy dla ryswanych obiektow
 	gluQuadricDrawStyle(sphere, GLU_FILL);
 	gluQuadricTexture(sphere, GL_TRUE);
 	gluQuadricNormals(sphere, GLU_SMOOTH);
@@ -782,16 +796,13 @@ void ChangeSize(GLsizei horizontal, GLsizei vertical)
 	glLoadIdentity();
 }
 
-void dayByDay() {
+void dayByDay() {//funkcja w tle zapeniajaca uplyw czasu
 	hour += speed;
 	Sleep(10);
 	RenderScene();
 
-	if (day >= INT_MAX - 5) {
-		day = INT_MIN + 5;
-	}
-	if (day <= INT_MIN + 5) {
-		day = INT_MAX - 5;
+	if (day >= INT_MAX - 5 || day <= INT_MIN + 5) {//zabezpieczenie przed przepelnieniem
+		day = 0;
 	}
 }
 
@@ -801,7 +812,7 @@ void main(void)
 	srand(time(NULL));
 	start = clock();
 
-	for (int i = 0; i < 3600; i++) {
+	for (int i = 0; i < 3600; i++) {//policzenie wspolrzednyhc dla pierscieni saturna
 		float angle = 1.0 * i / 3600;
 		ring[i] = sin(2 * M_PI * angle);
 	}
@@ -817,6 +828,6 @@ void main(void)
 	glutMotionFunc(Motion);//"lapanie" ruchu myszki
 	glutKeyboardFunc(keys);//"lapanie" akcji na klawiaturze
 	glEnable(GL_DEPTH_TEST);
-	glutIdleFunc(dayByDay);
+	glutIdleFunc(dayByDay);//funkcja w tle 
 	glutMainLoop();
 }
